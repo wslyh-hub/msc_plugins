012f8937f95b467e57b119c7f25383b76ee68511
fankang <1552951836@qq.com>
2025-02-16 13:48:55 +0800

    first commit

---
+++ MscPluginNpcLove.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using HarmonyLib;
+
+namespace Mcs_Refresh_NpcLove
+{
+    [BepInPlugin("wslyh","msc_refresh_npclove","1.0.0")]
+    public class MscPluginNpcLove : BaseUnityPlugin
+    {
+        void Start()
+        {
+            Logger.LogInfo("[msc refresh npclove]");
+            Harmony.CreateAndPatchAll(typeof(MscPluginNpcLove),null);
+        }
+        void Update()
+        {
+          
+        }
+
+
+        public static void  refresh_npc_love(JiaoYi.NpcLove curr_npc)
+        {
+            if(curr_npc != null)
+            {
+                int npc_id = JiaoYi.JiaoYiUIMag.Inst.NpcId;
+
+                jsonData.instance.MonstarCreatInterstingType(npc_id);
+
+                Text[] list = curr_npc.Parent.GetComponentsInChildren<Text>();
+
+                for (int i = 0; i < list.Length; i++)
+                {
+                    list[i].gameObject.SetActive(false);
+                }
+                curr_npc.InitNpcLove(npc_id);
+            }
+        }
+
+
+        public static void add_click(JiaoYi.NpcLove curr_npc)
+        {
+            Image love_img = curr_npc.GetComponentInChildren<Image>(); 
+
+            EventTrigger.Entry click = new EventTrigger.Entry
+            {
+                eventID = EventTriggerType.PointerClick
+            };
+
+            click.callback.AddListener(delegate (BaseEventData data)
+            {
+                refresh_npc_love(curr_npc);
+            });
+
+            love_img.gameObject.AddComponent<EventTrigger>().triggers.Add(click);
+        }
+    
+        [HarmonyPostfix, HarmonyPatch(typeof(JiaoYi.JiaoYiUIMag), "InitNpcData")]
+        private static void JiaoYiUIMag_Awake_Patch()
+        {
+            JiaoYi.NpcLove curr_npc = JiaoYi.JiaoYiUIMag.Inst.GetComponentInChildren<JiaoYi.NpcLove>();
+            if (curr_npc != null)
+            {
+                add_click(curr_npc);
+            }
+           
+        }
+    }
+}
